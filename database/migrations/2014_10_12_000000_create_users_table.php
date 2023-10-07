58cfdb065f8b4e05e424e62ee733c62529df7f0c
Samuel Sihotang <iss21041@students.del.ac.id>
2023-10-07 23:13:45 +0700

    made codename and name_slug to unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('codename');
-            $table->string('name_slug');
+            $table->string('codename')->unique();
+            $table->string('name_slug')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
