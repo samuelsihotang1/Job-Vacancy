9422a849ee0e27837e4dba47338b712359d567a9
Samuel Sihotang <iss21041@students.del.ac.id>
2023-11-17 15:52:28 +0700

    final

--- config/services.php
+++ config/services.php
@@ -37,4 +37,15 @@ return [
         'redirect' => 'http://localhost:8000/auth/google/callback',
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'), 
+        'redirect' => 'http://localhost:8000/auth/facebook/callback',
+    ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'), 
+        'redirect' => 'http://localhost:8000/auth/github/callback',
+    ],
 ];
